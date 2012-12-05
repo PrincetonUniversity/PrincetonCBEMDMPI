284977a71d9ca62be6097c66ea50f5417b3b1cbf
Nathan A. Mahynski <nathan.mahynski@gmail.com>
2012-12-05 08:27:25 -0500

    Added interaction.h back

--- code/interaction.cpp
+++ code/interaction.cpp
@@ -30,7 +30,7 @@ void force_serial (Atom *atom1, Atom *atom2, const vector <double> *box) {
  \param [in] \*a2 Pointer to atom 2
  \param [in] \*box Pointer to vector of cartesian box size
 */
-inline vector <double> Interaction::force (const Atom *a1, const Atom *a2, const vector <double> *box) {
+vector <double> Interaction::force (const Atom *a1, const Atom *a2, const vector <double> *box) {
 	// compute min image distance
 	double xyz[3];
 	double d2 = min_image_dist2 (a1, a2, box, xyz);
@@ -47,7 +47,7 @@ inline vector <double> Interaction::force (const Atom *a1, const Atom *a2, const
  \param [in] \*a2 Pointer to atom 2
  \param [in] \*box Pointer to vector of cartesian box size
  */
-inline double Interaction::energy (const Atom *a1, const Atom *a2, const vector <double> *box) {
+double Interaction::energy (const Atom *a1, const Atom *a2, const vector <double> *box) {
 	// compute min image distance
 	double xyz[3];
 	double d2 = min_image_dist2 (a1, a2, box, xyz);
